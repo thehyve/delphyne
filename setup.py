02fa480899e351a7933ba0b3bacac303d63ad512
Spayralbe <stefan@thehyve.nl>
2020-05-28 18:35:52 +0200

    parameterize setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages  # type: ignore
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# To update the package version number, edit __version__.py
+version = {}
+with open(os.path.join(here, '__version__.py')) as f:
+    exec(f.read(), version)
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
+with open('requirements.txt', 'r') as f:
+    required_packages = f.read().splitlines()
+
 setup(
     author="The Hyve",
     author_email="office@thehyve.nl",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
@@ -16,8 +27,12 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     description="Wrapper for OMOP ETL projects",
-    entry_points={"console_scripts": ["omop_etl_wrapper=omop_etl_wrapper.cli:main",],},
-    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "omop_etl_wrapper=omop_etl_wrapper.cli:main",
+        ],
+    },
+    install_requires=required_packages,
     license="GNU General Public License v3",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -27,8 +42,10 @@ setup(
     name="omop_etl_wrapper",
     package_dir={"": "src"},
     packages=find_packages(include=["src/omop_etl_wrapper", "src/omop_etl_wrapper.*"]),
-    setup_requires=[],
+    tests_require=[
+        'pytest',
+    ],
     url="https://github.com/thehyve/omop_etl_wrapper",
-    version="0.1.0",
+    version=version['__version__'],
     zip_safe=False,
 )
