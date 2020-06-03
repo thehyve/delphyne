b4172199eaee4079df48a718a91adafb4ac3864b
Spayralbe <stefan@thehyve.nl>
2020-06-04 00:26:34 +0200

    Add CDM ORM

---
+++ src/omop_etl_wrapper/cdm/hybrid/__init__.py
@@ -0,0 +1,61 @@
+# OMOP CDM Version 6 with oncology extension, and death table from v5.3.1.
+# Generated with python using sqlacodegen package on 2019-10-21,
+#  model from https://github.com/OHDSI/CommonDataModel/tree/Dev/PostgreSQL, commit 30d851a.
+
+from .clinical_data import (
+    ConditionOccurrence,
+    Person,
+    Death,
+    StemTable,
+    ProcedureOccurrence,
+    VisitOccurrence,
+    DeviceExposure,
+    DrugExposure,
+    Episode,
+    EpisodeEvent,
+    FactRelationship,
+    Measurement,
+    Note,
+    NoteNlp,
+    Observation,
+    ObservationPeriod,
+    Specimen,
+    SurveyConduct,
+    VisitDetail,
+)
+
+from .derived_elements import (
+    ConditionEra,
+    DoseEra,
+    DrugEra,
+)
+
+from .health_economics import (
+    Cost,
+    PayerPlanPeriod,
+)
+
+from .health_system_data import (
+    CareSite,
+    Location,
+    LocationHistory,
+    Provider,
+)
+
+from .vocabularies import (
+    Vocabulary,
+    SourceToConceptMap,
+    Concept,
+    ConceptAncestor,
+    ConceptClass,
+    ConceptRelationship,
+    ConceptSynonym,
+    Domain,
+    DrugStrength,
+    Relationship,
+)
+
+# __all__ = [
+#     a,
+#     b,
+# ]
---
+++ src/omop_etl_wrapper/cdm/hybrid/health_system_data.py
@@ -0,0 +1,98 @@
+# Copyright 2019 The Hyve
+#
+# Licensed under the GNU General Public License, version 3,
+# or (at your option) any later version (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.gnu.org/licenses/
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# OMOP CDM v6, with oncology extension, defined by https://github.com/OHDSI/CommonDataModel/tree/Dev/PostgreSQL #30d851a
+
+# coding: utf-8
+
+from sqlalchemy import BigInteger, Column, Date, ForeignKey, Integer, Numeric, String
+from sqlalchemy.orm import relationship
+
+from omop_etl_wrapper import base
+
+
+class CareSite(base):
+    __tablename__ = 'care_site'
+    __table_args__ = {'schema': 'cdm'}
+
+    care_site_id = Column(BigInteger, primary_key=True)
+    care_site_name = Column(String(255))
+    place_of_service_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+    location_id = Column(ForeignKey('cdm.location.location_id'))
+    care_site_source_value = Column(String(50))
+    place_of_service_source_value = Column(String(50))
+
+    location = relationship('Location')
+    place_of_service_concept = relationship('Concept')
+
+
+class Location(base):
+    __tablename__ = 'location'
+    __table_args__ = {'schema': 'cdm'}
+
+    location_id = Column(BigInteger, primary_key=True)
+    address_1 = Column(String(50))
+    address_2 = Column(String(50))
+    city = Column(String(50))
+    state = Column(String(2))
+    zip = Column(String(9))
+    county = Column(String(20))
+    country = Column(String(100))
+    location_source_value = Column(String(50))
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+    region_concept_id = Column(ForeignKey('vocab.concept.concept_id'))
+
+    region_concept = relationship('Concept')
+
+
+class LocationHistory(base):
+    __tablename__ = 'location_history'
+    __table_args__ = {'schema': 'cdm'}
+
+    location_history_id = Column(BigInteger, primary_key=True)
+    location_id = Column(ForeignKey('cdm.location.location_id'), nullable=False)
+    relationship_type_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+    domain_id = Column(String(50), nullable=False)
+    entity_id = Column(BigInteger, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date)
+
+    location = relationship('Location')
+    relationship_type_concept = relationship('Concept')
+
+
+class Provider(base):
+    __tablename__ = 'provider'
+    __table_args__ = {'schema': 'cdm'}
+
+    provider_id = Column(BigInteger, primary_key=True)
+    provider_name = Column(String(255))
+    npi = Column(String(20))
+    dea = Column(String(20))
+    specialty_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+    care_site_id = Column(ForeignKey('cdm.care_site.care_site_id'))
+    year_of_birth = Column(Integer)
+    gender_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+    provider_source_value = Column(String(50))
+    specialty_source_value = Column(String(50))
+    specialty_source_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+    gender_source_value = Column(String(50))
+    gender_source_concept_id = Column(ForeignKey('vocab.concept.concept_id'), nullable=False)
+
+    care_site = relationship('CareSite')
+    gender_concept = relationship('Concept', primaryjoin='Provider.gender_concept_id == Concept.concept_id')
+    gender_source_concept = relationship('Concept', primaryjoin='Provider.gender_source_concept_id == Concept.concept_id')
+    specialty_concept = relationship('Concept', primaryjoin='Provider.specialty_concept_id == Concept.concept_id')
+    specialty_source_concept = relationship('Concept', primaryjoin='Provider.specialty_source_concept_id == Concept.concept_id')
