472eba7296ede8f7d7b6cf28981cf25245d1a1a9
Spayralbe <stefan@thehyve.nl>
2020-06-04 00:25:21 +0200

    Update test stub

--- tests/test_omop_etl_wrapper.py
+++ tests/test_omop_etl_wrapper.py
@@ -1,5 +1,5 @@
-from omop_etl_wrapper import omop_etl_wrapper
+from omop_etl_wrapper import wrapper
 
 
 def test_omop_etl_wrapper():
-    assert omop_etl_wrapper is not None
+    assert wrapper is not None
