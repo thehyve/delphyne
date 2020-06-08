4dd15fc123ce0d4bffceb041c847f221a375b24a
Spayralbe <stefan@thehyve.nl>
2020-06-08 18:54:53 +0200

    Configure default schema names in one place

---
+++ src/omop_etl_wrapper/cdm/_defaults.py
@@ -0,0 +1,8 @@
+"""Module containing the default schema names as used in the ORM."""
+
+# These are the default schema names as used in the ORM table
+# definitions and as keys in the schema_translate_map.
+# Actual schema names can be different at runtime, as determined by the
+# provided parameters when instantiating the Wrapper class
+VOCAB_SCHEMA = 'vocab'
+CDM_SCHEMA = 'cdm'
