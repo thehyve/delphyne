110af2362ef5f8317b16e5c52c7c45ca3500f385
Spayralbe <stefan@thehyve.nl>
2020-10-14 18:01:53 +0200

    Add read csv methods

--- src/omop_etl_wrapper/model/source_data/source_file.py
+++ src/omop_etl_wrapper/model/source_data/source_file.py
@@ -1,7 +1,9 @@
+import csv
 import logging
+from copy import deepcopy
 from inspect import signature
 from pathlib import Path
-from typing import Dict, Optional, List, OrderedDict, Callable
+from typing import Dict, Optional, List, OrderedDict, Callable, Generator
 
 import pandas as pd
 
@@ -12,6 +14,11 @@ logger = logging.getLogger(__name__)
 
 _READ_CSV_PARAMS = signature(pd.read_csv).parameters
 _READ_SAS_PARAMS = signature(pd.read_sas).parameters
+
+_CSV_DIALECT_PARAMS = {kw for kw in dir(csv.Dialect)
+                       if not kw.startswith('_')}
+_CSV_DICT_READER_PARAMS = {'fieldnames', 'restkey', 'restval', 'dialect'}
+_FULL_CSV_PARAMS = _CSV_DIALECT_PARAMS.union(_CSV_DICT_READER_PARAMS)
 
 
 class SourceFile:
@@ -39,6 +46,23 @@ class SourceFile:
         logger.info(f'Retrieving {self._path.name} DataFrame from cache')
         return self._df
 
+    def _cache_df_copy(self, df: pd.DataFrame):
+        logger.info('Caching deepcopy of DataFrame')
+        self._df = df.copy(deep=True)
+
+    def _remove_cached_csv(self) -> None:
+        if self._csv:
+            logger.info(f'Removing cached csv records of {self.path.name}')
+            self._csv = []
+
+    def _retrieve_cached_csv(self):
+        logger.info(f'Retrieving {self._path.name} csv records from cache')
+        return self._csv
+
+    def _cache_csv_copy(self, csv_records: List[OrderedDict]):
+        logger.info('Caching deepcopy of csv records')
+        self._csv = deepcopy(csv_records)
+
     def get_csv_as_df(self,
                       apply_dtypes: bool,
                       force_reload: bool = False,
@@ -63,10 +87,6 @@ class SourceFile:
         """
         return self._get_df(read_func=self._read_csv_as_df, apply_dtypes=apply_dtypes,
                             force_reload=force_reload, cache=cache, **kwargs)
-
-    def _cache_df_copy(self, df: pd.DataFrame):
-        logger.info('Caching deepcopy of DataFrame')
-        self._df = df.copy(deep=True)
 
     def cache_df(self, df: pd.DataFrame) -> None:
         """
@@ -112,9 +132,7 @@ class SourceFile:
         config_kwargs = {kw: self._params.get(kw) for kw in self._params
                          if kw in _READ_CSV_PARAMS}
         full_kwargs = {**config_kwargs, **kwargs}
-        if full_kwargs.get('delimiter') is None or full_kwargs.get('encoding') is None:
-            raise ValueError(f'Cannot read {self._path.name} as DataFrame without providing '
-                             f'both the encoding and a delimiter')
+        self._check_missing_params(params=full_kwargs, required=['delimiter', 'encoding'])
         df = pd.read_csv(self._path, dtype='object', **full_kwargs)
         if apply_dtypes:
             df = self._apply_dtypes(df)
@@ -124,13 +142,17 @@ class SourceFile:
         config_kwargs = {kw: self._params.get(kw) for kw in self._params
                          if kw in _READ_SAS_PARAMS}
         full_kwargs = {**config_kwargs, **kwargs}
-        if full_kwargs.get('encoding') is None:
-            raise ValueError(f'Cannot read {self._path.name} as DataFrame without providing '
-                             f'the encoding')
+        self._check_missing_params(params=full_kwargs, required=['encoding'])
         df = pd.read_sas(self._path, **full_kwargs)
         if apply_dtypes:
             df = self._apply_dtypes(df)
         return df
+
+    def _check_missing_params(self, params: Dict, required: List[str]) -> None:
+        missing = [kw for kw in required if params.get(kw) is None]
+        if missing:
+            raise ValueError(f'Cannot read {self._path.name}, missing required '
+                             f'parameters: {", ".join(missing)}')
 
     def get_sas_as_df(self,
                       apply_dtypes: bool,
@@ -194,3 +216,65 @@ class SourceFile:
             logger.warning(f'No binary field available for for file {self.path.name} '
                            f'in the source config. Skipping line count calculation.')
         return None
+
+    def get_csv_as_dict_generator(self, **kwargs) -> Generator[OrderedDict, None, None]:
+        """
+        Return delimited text file as a generator of OrderedDicts.
+
+        Reading in this way keeps memory use very low, but you can only
+        access one row at a time.
+
+        :param kwargs:
+            Additional keyword arguments are passed on directly to
+            the csv module's DictReader.
+        :return: OrderedDict generator
+        """
+        logger.info(f'Reading {self._path.name} as csv records')
+        full_kwargs = {**self._params, **kwargs}
+        self._check_missing_params(params=full_kwargs, required=['delimiter', 'encoding'])
+        dict_reader_params = {kw: full_kwargs.get(kw) for kw in full_kwargs
+                              if kw in _FULL_CSV_PARAMS}
+
+        with self.path.open('r', encoding=full_kwargs['encoding']) as f:
+            reader = csv.DictReader(f, **dict_reader_params)
+            for row in reader:
+                yield row
+
+    def get_csv_as_list_of_dicts(self,
+                                 cache: bool = False,
+                                 **kwargs
+                                 ) -> List[OrderedDict]:
+        """
+        Return a delimited text file as a list of OrderedDicts.
+
+        This method loads the entire file into memory, meaning you can
+        access all rows in any order.
+
+        :param cache:
+            If True, keep the full list of dictionaries in memory for
+            future use.
+        :param kwargs:
+            Additional keyword arguments are passed on directly to
+            the csv module's DictReader.
+        :return: List[OrderedDict]
+        """
+
+        if self._csv:
+            csv_records = self._retrieve_cached_csv()
+        else:
+            logger.info(f'Reading {self._path.name} as csv records')
+            full_kwargs = {**self._params, **kwargs}
+            self._check_missing_params(params=full_kwargs, required=['delimiter', 'encoding'])
+            dict_reader_params = {kw: full_kwargs.get(kw) for kw in full_kwargs
+                                  if kw in _FULL_CSV_PARAMS}
+
+            with self.path.open('r', encoding=full_kwargs['encoding']) as f:
+                reader = csv.DictReader(f, **dict_reader_params)
+                csv_records = [row for row in reader]
+
+        if cache:
+            self._cache_csv_copy(csv_records)
+        else:
+            self._remove_cached_csv()
+
+        return csv_records
