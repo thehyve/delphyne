ec97dbbcd2ffbf9d4c4a1894d8e3a3ebad6991b1
Alessia Peviani <alessia@thehyve.nl>
2020-11-04 12:17:27 +0100

    Improve VocabLoader comments

--- src/omop_etl_wrapper/model/vocabulary_loader.py
+++ src/omop_etl_wrapper/model/vocabulary_loader.py
@@ -6,6 +6,10 @@ from ..database import Database
 from ..util.io import is_hidden
 import pandas as pd
 import numpy as np
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class VocabularyLoader:
@@ -28,7 +32,7 @@ class VocabularyLoader:
         # based on the file name conventions (e.g. "concept")
         return [f for f in self._custom_vocab_files if f.stem.endswith(omop_table)]
 
-    def _get_loaded_vocab_versions(self):
+    def _get_existing_vocab_versions(self):
         with self.db.session_scope() as session:
             vocabs = session.query(self._cdm.Vocabulary).all()
             return {v.vocabulary_id: v.vocabulary_version for v in vocabs}
@@ -38,33 +42,38 @@ class VocabularyLoader:
         # TODO: quality checks: mandatory fields, dependencies
         # self.check_custom_vocabularies_format()
 
-        existing_vocabs = self._get_loaded_vocab_versions()
+        # TODO: not sure I need this for anything, see how it is implemented in
+        #  check_if_existing_vocab_version(), check_if_existing_custom_class()
+        # existing_vocabs = self._get_existing_vocab_versions()
 
-        vocab_ids, vocab_files = self.get_custom_vocabulary_ids_and_files()
-        class_ids, class_files = self.get_custom_class_ids_and_files()
+        # get vocabularies and classes that need to be updated
+        vocab_ids, vocab_files = self._get_new_custom_vocabulary_ids_and_files()
+        class_ids, class_files = self._get_custom_class_ids_and_files()
 
         # TODO: remove/reapply constraints where needed
         # TODO: drop/load custom vocabulary concept id, if != 0
-        # drop older versions
-        self.drop_custom_concepts(vocab_ids)
-        self.drop_custom_vocabularies(vocab_ids)
-        self.drop_custom_classes(class_ids)
-        # load new versions
-        self.load_custom_classes(class_ids, class_files)
-        self.load_custom_vocabularies(vocab_ids, vocab_files)
-        self.load_custom_concepts(vocab_ids)
+        # drop older version
+        self._drop_custom_concepts(vocab_ids)
+        self._drop_custom_vocabularies(vocab_ids)
+        self._drop_custom_classes(class_ids)
+        # load new version
+        self._load_custom_classes(class_ids, class_files)
+        self._load_custom_vocabularies(vocab_ids, vocab_files)
+        self._load_custom_concepts(vocab_ids)
         # TODO: remove obsolete versions (i.e. cleanup in case of renaming of vocabs/classes);
         #  if the name has been changed, the previous drop won't find them;
         #  NOTE: for this to work, you need to keep a list of valid Athena vocabulary ids
         #  and check that no unknown vocabulary is present (not in Athena or custom vocab files);
         #  the cleanup could be rather time-consuming and should not be executed every time
-        valid_vocabs = self.get_list_of_valid_vocabularies()
-        self.drop_unused_custom_concepts(valid_vocabs)
-        self.drop_unused_custom_vocabularies(valid_vocabs)
-        valid_classes = self.get_list_of_valid_classes()
-        self.drop_unused_custom_classes(valid_classes)
+        valid_vocabs = self._get_list_of_valid_vocabularies()
+        self._drop_unused_custom_concepts(valid_vocabs)
+        self._drop_unused_custom_vocabularies(valid_vocabs)
+        valid_classes = self._get_list_of_valid_classes()
+        self._drop_unused_custom_classes(valid_classes)
 
-    def get_custom_vocabulary_ids_and_files(self):
+    def _get_new_custom_vocabulary_ids_and_files(self):
+
+        logging.info('Looking for new custom vocabulary versions')
 
         vocab_ids = set()
         vocab_files = set()
@@ -76,15 +85,18 @@ class VocabularyLoader:
                 vocab_id = row['vocabulary_id']
                 vocab_version = row['vocabulary_version']
 
-                if self.check_if_existing_vocab_version(vocab_id, vocab_version):
+                # skip loading if vocabulary version already present
+                if self._check_if_existing_vocab_version(vocab_id, vocab_version):
                     continue
 
                 vocab_ids.add(vocab_id)
                 vocab_files.add(vocab_file)
 
+        logging.info(f'Found vocabularies: {vocab_ids}')
+
         return list(vocab_ids), list(vocab_files)
 
-    def check_if_existing_vocab_version(self, vocab_id, vocab_version):
+    def _check_if_existing_vocab_version(self, vocab_id, vocab_version):
 
         with self.db.session_scope() as session:
             existing_record = \
@@ -94,7 +106,9 @@ class VocabularyLoader:
                     .one_or_none()
             return False if not existing_record else True
 
-    def get_custom_class_ids_and_files(self):
+    def _get_custom_class_ids_and_files(self):
+
+        logging.info('Looking for new custom class versions')
 
         class_ids = set()
         class_files = set()
@@ -106,15 +120,18 @@ class VocabularyLoader:
                 class_name = row['concept_class_name']
                 class_concept_id = row['concept_class_concept_id']
 
-                if self.check_if_existing_custom_class(class_id, class_name, class_concept_id):
+                # skip loading if class version already present
+                if self._check_if_existing_custom_class(class_id, class_name, class_concept_id):
                     continue
 
                 class_ids.add(class_id)
                 class_files.add(class_file)
 
+        logging.info(f'Found classes: {class_ids}')
+
         return list(class_ids), list(class_files)
 
-    def check_if_existing_custom_class(self, class_id, class_name, class_concept_id):
+    def _check_if_existing_custom_class(self, class_id, class_name, class_concept_id):
 
         with self.db.session_scope() as session:
             existing_record = \
@@ -125,7 +142,10 @@ class VocabularyLoader:
                 .one_or_none()
             return False if not existing_record else True
 
-    def drop_custom_concepts(self, vocab_ids):
+    def _drop_custom_concepts(self, vocab_ids):
+
+        logging.info(f'Dropping old custom concepts: '
+                     f'{True if vocab_ids else False}')
 
         if vocab_ids:
             with self.db.session_scope() as session:
@@ -133,7 +153,10 @@ class VocabularyLoader:
                     .filter(self._cdm.Concept.vocabulary_id.in_(vocab_ids)) \
                     .delete(synchronize_session='fetch')
 
-    def drop_custom_vocabularies(self, vocab_ids):
+    def _drop_custom_vocabularies(self, vocab_ids):
+
+        logging.info(f'Dropping old custom vocabulary versions: '
+                     f'{True if vocab_ids else False}')
 
         if vocab_ids:
             with self.db.session_scope() as session:
@@ -141,7 +164,10 @@ class VocabularyLoader:
                     .filter(self._cdm.Vocabulary.vocabulary_id.in_(vocab_ids)) \
                     .delete(synchronize_session='fetch')
 
-    def drop_custom_classes(self, class_ids):
+    def _drop_custom_classes(self, class_ids):
+
+        logging.info(f'Dropping old custom concept class versions: '
+                     f'{True if class_ids else False}')
 
         if class_ids:
             with self.db.session_scope() as session:
@@ -149,7 +175,10 @@ class VocabularyLoader:
                     .filter(self._cdm.ConceptClass.concept_class_id.in_(class_ids)) \
                     .delete(synchronize_session='fetch')
 
-    def load_custom_classes(self, class_ids, class_files):
+    def _load_custom_classes(self, class_ids, class_files):
+
+        logging.info(f'Loading new custom class versions: '
+                     f'{True if class_ids else False}')
 
         if class_ids:
 
@@ -168,7 +197,10 @@ class VocabularyLoader:
                         ))
                     session.add_all(records)
 
-    def load_custom_vocabularies(self, vocab_ids, vocab_files):
+    def _load_custom_vocabularies(self, vocab_ids, vocab_files):
+
+        logging.info(f'Loading new custom vocabulary versions: '
+                     f'{True if vocab_ids else False}')
 
         if vocab_ids:
 
@@ -189,7 +221,10 @@ class VocabularyLoader:
                         ))
                     session.add_all(records)
 
-    def load_custom_concepts(self, vocab_ids):
+    def _load_custom_concepts(self, vocab_ids):
+
+        logging.info(f'Loading new custom concept_ids: '
+                     f'{True if vocab_ids else False}')
 
         if vocab_ids:
 
@@ -217,17 +252,17 @@ class VocabularyLoader:
                         ))
                     session.add_all(records)
 
-    def get_list_of_valid_vocabularies(self):
+    def _get_list_of_valid_vocabularies(self):
         pass
 
-    def get_list_of_valid_classes(self):
+    def _get_list_of_valid_classes(self):
         pass
 
-    def drop_unused_custom_concepts(self, vocab_ids):
+    def _drop_unused_custom_concepts(self, vocab_ids):
         pass
 
-    def drop_unused_custom_vocabularies(self, vocab_ids):
+    def _drop_unused_custom_vocabularies(self, vocab_ids):
         pass
 
-    def drop_unused_custom_classes(self, class_ids):
+    def _drop_unused_custom_classes(self, class_ids):
         pass
