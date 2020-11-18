9f4a0ad1e83105b6f7296432b8a424cffed6f8de
Spayralbe <stefan@thehyve.nl>
2020-11-18 13:26:27 +0100

    Update noxfile
    Test for python versions 3.7-3.9
    For now use only flake8 for linting
    Fix generation of coverage reports via pytest-cov

--- noxfile.py
+++ noxfile.py
@@ -3,50 +3,37 @@ from pathlib import Path
 
 nox.options.sessions = ["tests", "lint", "build"]
 
-python = ["3.8"]
+python = ["3.7", "3.8", "3.9"]
 
 
 lint_dependencies = [
-    "-e",
-    ".",
-    "black",
     "flake8",
     "flake8-bugbear",
-    "mypy",
     "check-manifest",
 ]
 
 
 @nox.session(python=python)
 def tests(session):
-    session.install("-e", ".", "pytest", "pytest-cov")
-    tests = session.posargs or ["tests"]
-    session.run(
-        "pytest",
-        "--cov=omop_etl_wrapper",
-        "--cov-config",
-        ".coveragerc",
-        "--cov-report=",
-        *tests
-    )
+    session.install(".", "pytest", "pytest-cov", "docker")
+    session.run("pytest", "--cov=src")
     session.notify("cover")
 
 
 @nox.session
 def cover(session):
-    """Coverage analysis"""
+    """Coverage analysis."""
     session.install("coverage")
-    session.run("coverage", "report", "--show-missing", "--fail-under=0")
+    session.run("coverage", "report", "--show-missing", "--fail-under=85")
     session.run("coverage", "erase")
 
 
 @nox.session(python="3.8")
 def lint(session):
+    """Lint using flake8."""
     session.install(*lint_dependencies)
-    files = ["tests"] + [str(p) for p in Path(".").glob("*.py")]
-    session.run("black", "--check", *files)
+    files = ["tests", "src"] + [str(p) for p in Path(".").glob("*.py")]
     session.run("flake8", *files)
-    session.run("mypy", *files)
     session.run("python", "setup.py", "check", "--metadata", "--strict")
     if "--skip_manifest_check" in session.posargs:
         pass
