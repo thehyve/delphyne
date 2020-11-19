135bea23a826160186776ac6e4ab8cb314335502
Spayralbe <stefan@thehyve.nl>
2020-11-20 00:51:26 +0100

    Add logging tests

---
+++ tests/python/log/test_logging_context.py
@@ -0,0 +1,47 @@
+import io
+import logging
+from typing import List
+
+import pytest
+from src.omop_etl_wrapper.log.log_formats import MESSAGE_ONLY
+from src.omop_etl_wrapper.log.logging_context import LoggingFormatContext
+
+logger = logging.getLogger('test_logger')
+
+
+@pytest.fixture(scope='function')
+def string_stream_handler() -> logging.StreamHandler:
+    """Add a StringIO StreamHandler to logger and yield it.
+    Afterwards remove all logger handlers."""
+    string_io = io.StringIO()
+    stream_handler = logging.StreamHandler(string_io)
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    yield stream_handler
+    logger.handlers = []
+
+
+def get_stringio_lines(stringio: io.StringIO) -> List[str]:
+    return stringio.getvalue().split('\n')
+
+
+def test_logging_context(string_stream_handler: logging.StreamHandler, caplog):
+    """logging_context allows temporarily changing the log format."""
+    dinosaur_format = logging.Formatter("🦕%(message)s🦕")
+    with caplog.at_level(logging.INFO):
+        logger.info('this has regular format')
+        with LoggingFormatContext(logger, dinosaur_format):
+            logger.info('THIS HAS AWESOME DINOSAUR FORMAT!')
+        logger.info('this has boring regular format again')
+    log_lines = get_stringio_lines(string_stream_handler.stream)
+    assert log_lines[0] == 'INFO - this has regular format'
+    assert log_lines[1] == '🦕THIS HAS AWESOME DINOSAUR FORMAT!🦕'
+    assert log_lines[2] == 'INFO - this has boring regular format again'
+
+
+def test_log_format_message_only(caplog):
+    with caplog.at_level(logging.INFO):
+        logger.info('back in my day logging was just about the message')
+    expected = 'back in my day logging was just about the message'
+    assert MESSAGE_ONLY.format(caplog.records[0]) == expected
