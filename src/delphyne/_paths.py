2d34ac4e8842f24a539d95e5acd8d3613ab74891
Spayralbe <stefan@thehyve.nl>
2021-01-12 23:31:55 +0100

    Update paths

--- src/delphyne/_paths.py
+++ src/delphyne/_paths.py
@@ -1,15 +1,14 @@
 """
-Container for all relative path constants, as they are expected to be
+Module for all relative path constants, as they are expected to be
 present in the template repository.
 """
-
-# TODO: Add expected paths for:
-#  omop vocabularies dir
 
 from pathlib import Path
 
 # Config file used by setup_logging
 LOG_CONFIG_PATH = Path('./config/logging.yml')
+
+# Config file for source data file properties
 SOURCE_DATA_CONFIG_PATH = Path('./config/source_config.yml')
 
 # All log files will be written here. Will be created if not present.
@@ -18,12 +17,12 @@ LOG_OUTPUT_DIR = Path('./logs')
 # Directory containing SQL scripts
 SQL_TRANSFORMATIONS_DIR = Path('./src/main/sql')
 
-RESOURCES_DIR = Path('./resources')
+VOCAB_DIR = Path('./resources/vocabularies')
 # Directory containing all source_to_concept_map files
-STCM_DIR = RESOURCES_DIR / 'source_to_concept'
+STCM_DIR = VOCAB_DIR / 'source_to_concept_map'
 # File containing the versioning info of the STCM mappings
 STCM_VERSION_FILE = STCM_DIR / 'stcm_versions.tsv'
 # Directory containing all custom vocabulary tables
-CUSTOM_VOCAB_DIR = RESOURCES_DIR / 'custom_vocabularies'
+CUSTOM_VOCAB_DIR = VOCAB_DIR / 'custom'
 # Directory containing all standard vocabulary tables
-STANDARD_VOCAB_DIR = RESOURCES_DIR / 'standard_vocabularies'
+STANDARD_VOCAB_DIR = VOCAB_DIR / 'standard'
