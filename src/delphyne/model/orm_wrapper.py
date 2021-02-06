2b92826f1fcbafd5abe6408777ddc918c52def33
Maxim Moinat <maxim@thehyve.nl>
2021-02-06 13:22:27 +0100

    do not pass session to transformation and some refactorings

--- src/delphyne/model/orm_wrapper.py
+++ src/delphyne/model/orm_wrapper.py
@@ -53,10 +53,11 @@ class OrmWrapper(ABC):
 
     def execute_batch_transformation(self, batch_statement: Callable, bulk: bool = False, batch_size: int = 10000) -> None:
         """
-        Execute an ETL transformation in batches of given size via a
-        python statement. Batches are committed to the database
-        independently and a failed insertion of a batch will
-        not trigger a rollback of the other batches.
+        Execute an ETL transformation statement in batches.
+
+        Batches are committed to the database independently and
+        a failed insertion of a batch will not trigger
+        a rollback of the other batches.
 
         Parameters
         ----------
@@ -78,43 +79,36 @@ class OrmWrapper(ABC):
         None
         """
         logger.info(f'Executing batched transformation: {batch_statement.__name__} ')
-        func_args = signature(batch_statement).parameters
-        if 'session' in func_args:
-            session = self.db.get_new_session()
-            records_generator = batch_statement(self, session)
-        else:
-            records_generator = batch_statement(self)
+
+        records_generator = batch_statement(self)
 
         records_to_insert = []
         n_batches_success = 0
-        n_batches_failed = 0
         total_records_inserted = 0
-        i = 0
+        batch_count = 0
         for record in records_generator:
             records_to_insert.append(record)
             if len(records_to_insert) >= batch_size:
-                i += 1
-                success = self._insert_records(records_to_insert, batch_statement.__name__ + str(i), bulk)
-                if success:
+                batch_count += 1
+                if self._insert_records(records_to_insert,
+                                        batch_statement.__name__ + str(batch_count),
+                                        bulk):
                     n_batches_success += 1
                     total_records_inserted += len(records_to_insert)
-                else:
-                    n_batches_failed += 1
                 records_to_insert = []
 
         # Insert any remaining records
         if len(records_to_insert) > 0:
-            i += 1
-            success = self._insert_records(records_to_insert, batch_statement.__name__ + str(i), bulk)
-            if success:
+            batch_count += 1
+            if self._insert_records(records_to_insert,
+                                    batch_statement.__name__ + str(batch_count),
+                                    bulk):
                 n_batches_success += 1
                 total_records_inserted += len(records_to_insert)
-            else:
-                n_batches_failed += 1
 
         logger.info(f'Saved a total of {total_records_inserted} records')
         logger.info(f'{batch_statement.__name__} batches completed with status: '
-                    f'{n_batches_success} success and {n_batches_failed} fails')
+                    f'{n_batches_success} success and {batch_count-n_batches_success} fails')
 
     def _insert_records(self, records_to_insert: List, name: str, bulk: bool) -> bool:
         with self.db.tracked_session_scope(name=name, raise_on_error=False) \
